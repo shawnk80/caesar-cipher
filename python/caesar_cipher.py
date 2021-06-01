ab72165e6d9c539d56f281720be3266aa3ff8d9f
Shawn Kiernan <kiernan.shawn@gmail.com>
2021-06-01 14:58:04 -0500

    Shawn's solution to caesar cipher in python

--- python/caesar_cipher.py
+++ python/caesar_cipher.py
@@ -1,1 +1,21 @@
+# given an input STRING and number of chars to shift SHIFT_AMOUNT
+# shift each alphabetic character left or right by SHIFT_AMOUNT
+# if SHIFT_AMOUNT < 0, shift left
+# if SHIFT_AMOUNT > 0, shift right
+# return the resulting SHIFTEDSTRING
+import string
+
 def caesar_cipher(string, shift_amount):
+    shiftedString = ""
+
+    for ch in string:
+        if (ch.isalpha()):
+            if (ch.islower()):
+                shiftedString += chr(((ord(ch) + shift_amount - ord('a')) % 26) + ord('a'))
+            else: 
+                shiftedString += chr(((ord(ch) + shift_amount - ord('A')) % 26) + ord('A'))
+        else:
+            shiftedString += ch
+    
+    return shiftedString
+
--- python/caesar_cipher_spec.py
+++ python/caesar_cipher_spec.py
@@ -1,7 +1,19 @@
 # Rewrite this in Unit Test
-
+import unittest
 from caesar_cipher import caesar_cipher
+class CaesarCipherTestCase(unittest.TestCase):
 
-print(caesar_cipher("Boy! What a string!", -5) == "Wjt! Rcvo v nomdib!")
-print(caesar_cipher("Hello zach168! Yes here.", 5) == "Mjqqt efhm168! Djx mjwj.")
-print(caesar_cipher("Hello Zach168! Yes here.", -5) == "Czggj Uvxc168! Tzn czmz.")
+    def test_case_1(self):
+        output = caesar_cipher("Boy! What a string!", -5)
+        self.assertEqual(output, "Wjt! Rcvo v nomdib!")
+
+    def test_case_2(self):
+        output = caesar_cipher("Hello zach168! Yes here.", 5)
+        self.assertEqual(output, "Mjqqt efhm168! Djx mjwj.")
+    
+    def test_case_3(self):
+        output = caesar_cipher("Hello zach168! Yes here.", -5)
+        self.assertEqual(output, "Czggj uvxc168! Tzn czmz.")
+
+if __name__ == '__main__':
+    unittest.main()
